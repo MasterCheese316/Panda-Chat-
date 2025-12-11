1ba43b8a3856636aba12329c6fa5ae0c9a1e9386
mastercheese316-source <mastercheese316@gmail.com>
2025-12-11 16:20:39 -0700

    Update severdot.js

--- severdot.js
+++ severdot.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.json());
 app.use(express.static('.'));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
 app.listen(8080, () => console.log('Listening on port 8080'));

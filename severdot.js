1904e03b4b15f336df38cb8aebf856a0362b93a4
mastercheese316-source <mastercheese316@gmail.com>
2025-12-11 16:43:48 -0700

    Update severdot.js

--- severdot.js
+++ severdot.js
@@ -1,6 +1,5 @@
-const path = require('path'); npm install && cd .. && node server.js
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.static('.'));
